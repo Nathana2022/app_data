c9e80930dac0c6ce0bbafacb3bc877bc140d083b
Nathana2022 <nathanaeldon495@gmail.com>
2024-02-20 18:26:00 +0100

    Style : Gestion design Intropage

--- lib/datasource/DataService.ts
+++ lib/datasource/DataService.ts
@@ -1,6 +1,38 @@
 import { Categorie } from "./models/Categories";
+import { Articles } from "./models/Articles";
 
 export class DataService {
+    static async recuperationArticles(){
+        return await new Promise(resolve => {
+            setTimeout(() => {
+                const responseServeur : Articles[] = [
+                    // {
+                    //     id: 1,
+                    //     name: "Ecolab - Power Fusion"
+                    //   }, 
+                    //   {
+                    //     id: 2,
+                    //     name: "Fudge - Cream Fudge"
+                    //   }, 
+                    //   {
+                    //     id: 3,
+                    //     name: "Jerusalem Artichoke"
+                    //   }, 
+                    //   {
+                    //     id: 4,
+                    //     name: "Eel - Smoked"
+                    //   }, 
+                    //   {
+                    //     id: 5,
+                    //     name: "Napkin White - Starched"
+                    //   }
+                    
+                    ];
+                resolve(responseServeur);
+            }),800
+        });
+    };
+
     static async recuperationCategorie(){
         return await new Promise(resolve =>{
             setTimeout(()=> {
--- lib/pages/intro/IntroPage.tsx
+++ lib/pages/intro/IntroPage.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import {Image, StyleSheet} from 'react-native';
-import {Text, TouchableOpacity, View} from 'react-native';
+import React from "react";
+import { Image, StyleSheet } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
 
 const IntroPage = (): React.JSX.Element => {
   return (
     <View style={styles.parent}>
-      <Image style={styles.logo} source={require('../../../images/tweet-profile-photo.png')} />
-      <Text>Somba Na Tshombo</Text>
+        <Text style={styles.textTitle}>Somba Na Tshombo</Text>
+      <Image style={styles.logo} source={require("../../../images/icon.png")} />
       <TouchableOpacity style={styles.button}>
         <Text style={styles.text}>Continuer</Text>
       </TouchableOpacity>
@@ -15,29 +15,34 @@ const IntroPage = (): React.JSX.Element => {
 };
 
 const styles = StyleSheet.create({
-    parent : {
-        flex:1,
-        backgroundColor: "whrite",
-        justifyContent: 'center', // Centrer horizontalement
-        alignItems: 'center', // Centrer verticalement
-        padding: 30,
-    },
-    logo : {
-        height:150,
-        width:150,
-    },
-    text : {
-        color: 'white',
-        fontSize: 16,
-        fontWeight: 'bold',
-    },
-    button : {
-        backgroundColor: '',
-        padding: 10,
-        borderRadius: 5,
-        marginTop: 10,
-        width:100
-    },
-})
+  parent: {
+    flex: 1,
+    backgroundColor: "white",
+    justifyContent: "center", // Centrer horizontalement
+    alignItems: "center", // Centrer verticalement
+    padding: 30
+  },
+  logo: {
+    height: 250,
+    width: 250
+  },
+  text: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "bold"
+  },
+  button: {
+    backgroundColor: "",
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 10,
+    width: 100
+  },
+  textTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "black"
+  }
+});
 
 export default IntroPage;
